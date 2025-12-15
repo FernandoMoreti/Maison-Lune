24f26897a2a387d8f2c2183a77ae0a4e5c11c46e
Fernando Moreti <fernandombolela@gmail.com>
2025-12-15 10:14:31 -0300

    feat(Frontend) Adicionando o Framer Motion

--- components/layout/About.tsx
+++ components/layout/About.tsx
@@ -11,9 +11,8 @@ export default function About() {
 		<motion.section
 			initial={{ opacity: 0, y: 60 }}
 			whileInView={{ opacity: 1, y: 0 }}
-			viewport={{ once: true, amount: 0.3 }}
+			viewport={{ once: true, amount: 0 }}
 			transition={{ duration: 0.6, ease: "easeOut" }}
-			className="py-20"
 		>
 			<section
 				id="about"
