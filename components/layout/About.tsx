ea71432698646af698a342648e26d3be267b4da8
Fernando Moreti <fernandombolela@gmail.com>
2026-01-11 20:01:21 -0300

    feat(mobile) Deixando o About responsivo

--- components/layout/About.tsx
+++ components/layout/About.tsx
@@ -16,12 +16,12 @@ export default function About() {
 		>
 			<section
 				id="about"
-				className="grid grid-cols-2 gap-10 bg-linear-to-l from-[#d1c3b5] to-[#ffff] px-15 py-10"
+				className="grid grid-cols-1 md:grid-cols-2 gap-10 bg-linear-to-l from-[#d1c3b5] to-[#ffff] px-15 py-10"
 			>
 				<div className="">
 					<div className="flex flex-col">
 						<p className="text-sm tracking-widest">SOBRE NÓS</p>
-						<h1 className="text-4xl md:text-5xl font-bold mt-4 mb-6">
+						<h1 className="text-center md:text-start text-4xl md:text-5xl font-bold mt-4 mb-6">
 							Paixão por
 							<span className="bg-linear-to-r from-yellow-900 via-yellow-300 to-yellow-900 bg-clip-text text-transparent text-gradient">
 								{" "}
@@ -29,13 +29,13 @@ export default function About() {
 							</span>
 							Femininos
 						</h1>
-						<p className="text-[#b08a69] mb-6 text-lg">
+						<p className="text-center md:text-start text-[#b08a69] mb-6 text-lg">
 							A<strong> Passos Elegantes </strong>nasceu do amor por calçados
 							femininos que combinam estilo, conforto e qualidade. Há mais de 8
 							anos, selecionamos os melhores modelos para mulheres que valorizam
 							cada detalhe.
 						</p>
-						<p className="text-[#b08a69] mb-8">
+						<p className="text-center md:text-start text-[#b08a69] mb-8">
 							Do salto alto clássico às sapatilhas confortáveis, cada par é
 							escolhido pensando em você. Trabalhamos com os melhores
 							fabricantes nacionais para garantir durabilidade e sofisticação em
@@ -47,7 +47,7 @@ export default function About() {
 						whileInView={{ opacity: 1, y: 0 }}
 						viewport={{ once: true, amount: 0.5 }}
 						transition={{ duration: 0.6 }}
-						className="grid grid-cols-2 gap-10"
+						className="grid grid-cols-1 md:grid-cols-2 gap-10"
 					>
 						<Card
 							icon={Heart}
@@ -82,7 +82,7 @@ export default function About() {
 					whileInView={{ opacity: 1, scale: 1 }}
 					viewport={{ once: true, amount: 0.5 }}
 					transition={{ duration: 0.5 }}
-					className="flex justify-center  rounded-4xl items-center bg-[#7e553c]"
+					className="hidden md:flex justify-center rounded-4xl items-center bg-[#7e553c]"
 				>
 					<Image src={"/Ativo 6.jpg"} alt="Logo" width={600} height={200} />
 				</motion.div>
